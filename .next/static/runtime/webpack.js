e5404c3678ed869ea9bdc0d59797d59f39e11d68
Amos Welike <amoswelike@gmail.com>
2020-03-02 02:33:09 +0300

    modify onsubmit

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "aa1155ef759d1bfd364d";
+/******/ 	var hotCurrentHash = "69b15b54eac87a16506d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
